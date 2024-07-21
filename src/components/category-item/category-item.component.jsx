73d4619715525d506ea63c139bacecaa899b9da4
Blackpossum <yosaphatharwindra@gmail.com>
2024-07-21 23:45:53 +0700

    feat: initiate routing using react router dom

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,18 +1,19 @@
 import './category-item.styless.scss'
+import jackets from '../../assets/adrian-ordonez-P0W27GRvyww-unsplash.jpg'
 
 const CategoryItem = ({ category }) => {
   // destructure props
-  const { imageUrl, title }= category
+  const { imageUrl, title, id }= category
   return (
     <div className="category-container" key={id}>
           <div className="background-image" style={{
-            backgroundImage: `url(${imageUrl})`
+            backgroundImage: `url(${jackets})`
           }}/> 
           <div className="category-body-container" key={id}>
             <h2>{title}</h2>
             <p>Shop Now</p>
           </div>
-        </div>
+    </div>
   )
 }
 
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -4,8 +4,8 @@ import './directory.styles.scss'
 const Directory = ({categories}) => {
   return (
     <div className="directory-container">
-        {categories.map((categories) => (
-        <CategoryItem category={categories} key={category.id}/>
+        {categories.map((category) => (
+        <CategoryItem category={category} key={category.id}/>
         ))}
     </div>
   )
